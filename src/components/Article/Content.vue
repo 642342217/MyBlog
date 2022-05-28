73c9e78c286e16a95e128bde42740cd014e43f2f
chenhuijun <642342217@qq.com>
2022-05-28 20:40:24 +0800

    增加动画效果

--- src/components/Article/Content.vue
+++ src/components/Article/Content.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="{ 'right': true, 'day': isDay }">
+  <div :class="{ right: true, day: isDay }">
     <div class="title">{{ article.title }}</div>
     <div class="description">
       <i class="iconfont">&#xe65c;<span> HUIJUN</span></i>
@@ -14,14 +14,16 @@
       <article v-html="article.content" class="markdown-body"></article>
     </div>
   </div>
-  <div class="top" @click="toTop"><i class="iconfont">&#xe635;</i></div>
+  <div class="top" :style="showTopStatus ? '' : 'top: 100vh'" @click="toTop">
+    <i class="iconfont">&#xe635;</i>
+  </div>
 </template>
 
 <script>
 import api from "../../api/index";
 import MarkdownIt from "markdown-it";
 import "../../assets/markdown.css";
-import { onBeforeMount, reactive, toRefs } from "vue";
+import { onBeforeMount, reactive, toRefs, ref } from "vue";
 import { useStore } from "vuex";
 import { useRoute } from "vue-router";
 export default {
@@ -30,6 +32,7 @@ export default {
     const article = reactive({});
     const route = useRoute();
     const currentRoute = reactive(useRoute());
+    let showTopStatus = ref(false);
     let { getIsDay: isDay } = toRefs(store.getters);
 
     const md = new MarkdownIt();
@@ -51,9 +54,26 @@ export default {
     //回到文章顶部
     function toTop() {
       window.scrollTo({
-        top: 0
-      })
+        top: 0,
+      });
     }
+
+    //根据滚动距离，展示滚动置顶图标
+    let flag = false;
+    window.addEventListener("scroll", function () {
+      if (flag) return;
+      flag = true;
+      setTimeout(() => {
+        let scrollY =
+          document.documentElement.scrollTop || document.body.scrollTop;
+        if (scrollY > 1000) {
+          showTopStatus.value = true;
+        } else {
+          showTopStatus.value = false;
+        }
+        flag = false;
+      }, 500);
+    });
 
     // 获取当前文章所在目录下的文章
     let titles = reactive([]);
@@ -75,7 +95,8 @@ export default {
       titles,
       route,
       isDay,
-      toTop
+      toTop,
+      showTopStatus,
     };
   },
 };
@@ -117,6 +138,7 @@ export default {
   }
 }
 .top {
+  transition: all 0.5s linear;
   position: fixed;
   z-index: 10000;
   right: 2rem;
@@ -132,34 +154,34 @@ export default {
 @media (max-width: 991px) {
   div.right {
     flex: 1 1 0%;
-  margin: 0 auto;
-  width: 100vw;
-  padding: 1.1rem 1.1rem;
-  box-sizing: border-box;
-  border-color: rgb(234, 236, 239);
-  background-color: rgb(255, 255, 255);
-  box-shadow: rgb(0 0 0 / 10%) 0px 1px 8px 0px;
-  &:hover {
-    box-shadow: 0 2px 16px 0 rgba(0, 0, 0, 0.2);
-  }
-  .title {
-    transition: 0.3s all;
-    position: relative;
-    font-size: 1.58rem;
-    font-weight: 700;
-    line-height: 46px;
-    display: inline-block;
-  }
-  .description {
-    i {
-      color: #666;
-      margin-right: 16px;
-    }
-  }
-  .content {
+    margin: 0 auto;
+    width: 100vw;
+    padding: 1.1rem 1.1rem;
     box-sizing: border-box;
-    padding: 1rem 0;
-  }
+    border-color: rgb(234, 236, 239);
+    background-color: rgb(255, 255, 255);
+    box-shadow: rgb(0 0 0 / 10%) 0px 1px 8px 0px;
+    &:hover {
+      box-shadow: 0 2px 16px 0 rgba(0, 0, 0, 0.2);
+    }
+    .title {
+      transition: 0.3s all;
+      position: relative;
+      font-size: 1.58rem;
+      font-weight: 700;
+      line-height: 46px;
+      display: inline-block;
+    }
+    .description {
+      i {
+        color: #666;
+        margin-right: 16px;
+      }
+    }
+    .content {
+      box-sizing: border-box;
+      padding: 1rem 0;
+    }
   }
 }
 .day {
